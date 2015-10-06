6c3eab451bb628f83d49b59809dcea52663d23fb
Chris Gomez <akagomez@gmail.com>
2015-10-06 13:52:05 -0400

    Cleanup examples

--- examples/filter/common.js
+++ examples/filter/common.js
@@ -1,3 +1,207 @@
+window.NUMBER_OF_CIRCLES = 500;
+
+window.numbersLib = {
+    generateRandomNumber: function () {
+        return Math.ceil(Math.random() * 99);
+    },
+    alternateNumber: window.alternateNumber = function (oldNum) {
+        var isEven = oldNum % 2 === 0;
+        var isOdd = ! isEven;
+        var isLessThan = oldNum < 50;
+        var isGreaterThan = ! isLessThan;
+        var rand = Math.round(Math.random() * 24); // 0 - 24
+
+        if (isEven && isLessThan) {
+            num = rand * 2 + 1 + 50; // Odd (51 - 99)
+        } else if (isEven && isGreaterThan) {
+            num = rand * 2 + 1; // Even (1 - 49)
+        } else if (isOdd && isLessThan) {
+            num = rand * 2 + 50; // Even (50 - 98)
+        } else if (isOdd && isGreaterThan) {
+            num = rand * 2; // Even (0 - 48)
+        }
+
+        return num;
+    }
+};
+
+
+
+
+/**
+ * @author mrdoob / http://mrdoob.com/
+ * @author jetienne / http://jetienne.com/
+ * @author paulirish / http://paulirish.com/
+ */
+var MemoryStats = function() {
+
+    var msMin = 100;
+    var msMax = 0;
+
+    var container = document.createElement('div');
+    container.id = 'stats';
+    container.style.cssText = 'width:80px;opacity:0.9;cursor:pointer';
+
+    var msDiv = document.createElement('div');
+    msDiv.id = 'ms';
+    msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#020;';
+    container.appendChild(msDiv);
+
+    var msText = document.createElement('div');
+    msText.id = 'msText';
+    msText.style.cssText = 'color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
+    msText.innerHTML = 'Memory';
+    msDiv.appendChild(msText);
+
+    var msGraph = document.createElement('div');
+    msGraph.id = 'msGraph';
+    msGraph.style.cssText = 'position:relative;width:74px;height:30px;background-color:#0f0';
+    msDiv.appendChild(msGraph);
+
+    while (msGraph.children.length < 74) {
+
+        var bar = document.createElement('span');
+        bar.style.cssText = 'width:1px;height:30px;float:left;background-color:#131';
+        msGraph.appendChild(bar);
+
+    }
+
+    var updateGraph = function(dom, height, color) {
+
+        var child = dom.appendChild(dom.firstChild);
+        child.style.height = height + 'px';
+        if (color) child.style.backgroundColor = color;
+
+    }
+
+    var perf = window.performance || {};
+    // polyfill usedJSHeapSize
+    if (!perf && !perf.memory) {
+        perf.memory = {
+            usedJSHeapSize: 0
+        };
+    }
+    if (perf && !perf.memory) {
+        perf.memory = {
+            usedJSHeapSize: 0
+        };
+    }
+
+    // support of the API?
+    if (perf.memory.totalJSHeapSize === 0) {
+        console.warn('totalJSHeapSize === 0... performance.memory is only available in Chrome .')
+    }
+
+    // TODO, add a sanity check to see if values are bucketed.
+    // If so, reminde user to adopt the --enable-precise-memory-info flag.
+    // open -a "/Applications/Google Chrome.app" --args --enable-precise-memory-info
+
+    var lastTime = Date.now();
+    var lastUsedHeap = perf.memory.usedJSHeapSize;
+    return {
+        domElement: container,
+
+        update: function() {
+
+            // refresh only 30time per second
+            if (Date.now() - lastTime < 1000 / 30) return;
+            lastTime = Date.now()
+
+            var delta = perf.memory.usedJSHeapSize - lastUsedHeap;
+            lastUsedHeap = perf.memory.usedJSHeapSize;
+            var color = delta < 0 ? '#830' : '#131';
+
+            var ms = perf.memory.usedJSHeapSize;
+            msMin = Math.min(msMin, ms);
+            msMax = Math.max(msMax, ms);
+            msText.textContent = "Mem: " + bytesToSize(ms, 2);
+
+            var normValue = ms / (30 * 1024 * 1024);
+            var height = Math.min(30, 30 - normValue * 30);
+            updateGraph(msGraph, height, color);
+
+            function bytesToSize(bytes, nFractDigit) {
+                var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+                if (bytes == 0) return 'n/a';
+                nFractDigit = nFractDigit !== undefined ? nFractDigit : 0;
+                var precision = Math.pow(10, nFractDigit);
+                var i = Math.floor(Math.log(bytes) / Math.log(1024));
+                return Math.round(bytes * precision / Math.pow(1024, i)) / precision + ' ' + sizes[i];
+            };
+        }
+
+    }
+
+};
+
+
+
+
+
+var Monitoring = Monitoring || (function() {
+
+    var stats = new MemoryStats();
+    stats.domElement.style.position = 'fixed';
+    stats.domElement.style.right = '0px';
+    stats.domElement.style.bottom = '0px';
+    document.body.appendChild(stats.domElement);
+    requestAnimationFrame(function rAFloop() {
+        stats.update();
+        requestAnimationFrame(rAFloop);
+    });
+
+    var RenderRate = function() {
+        var container = document.createElement('div');
+        container.id = 'stats';
+        container.style.cssText = 'width:150px;opacity:0.9;cursor:pointer;position:fixed;right:80px;bottom:0px;';
+
+        var msDiv = document.createElement('div');
+        msDiv.id = 'ms';
+        msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#020;';
+        container.appendChild(msDiv);
+
+        var msText = document.createElement('div');
+        msText.id = 'msText';
+        msText.style.cssText = 'color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
+        msText.innerHTML = 'Repaint rate: 0/sec';
+        msDiv.appendChild(msText);
+
+        var bucketSize = 20;
+        var bucket = [];
+        var lastTime = Date.now();
+        return {
+            domElement: container,
+            ping: function() {
+                var start = lastTime;
+                var stop = Date.now();
+                var rate = 1000 / (stop - start);
+                bucket.push(rate);
+                if (bucket.length > bucketSize) {
+                    bucket.shift();
+                }
+                var sum = 0;
+                for (var i = 0; i < bucket.length; i++) {
+                    sum = sum + bucket[i];
+                }
+                msText.textContent = "Repaint rate: " + (sum / bucket.length).toFixed(2) + "/sec";
+                lastTime = stop;
+            }
+        }
+    };
+
+    var renderRate = new RenderRate();
+    document.body.appendChild(renderRate.domElement);
+
+    return {
+        memoryStats: stats,
+        renderRate: renderRate
+    };
+
+})();
+
+
+
+
 // randomColor by David Merfield under the MIT license
 // https://github.com/davidmerfield/randomColor/
 
