e9152b24c34a7155de125e470ea117b700927a8f
Chris Gomez <akagomez@gmail.com>
2015-10-07 17:33:04 -0400

    Define can.DeriveList in main

--- can-derive.js
+++ can-derive.js
@@ -1,5 +1,9 @@
 var List = require('list/list');
 
+if (typeof window !== 'undefined' && !require.resolve && window.can) {
+    window.can.DeriveList = List;
+}
+
 module.exports = {
     List: List
 };
