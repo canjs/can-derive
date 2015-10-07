8b687666dd7b7fc33259122a473029e7e37abf04
Chris Gomez <akagomez@gmail.com>
2015-10-07 12:58:19 -0400

    Release, something

---
+++ build.js
@@ -0,0 +1,16 @@
+var stealTools = require("steal-tools");
+
+stealTools.export({
+    system: {
+        config: __dirname + "/package.json!npm"
+    },
+    outputs: {
+        "+amd": {},
+        "+global-js": {},
+        "+cjs": {}
+    }
+}).catch(function(e){
+    setTimeout(function(){
+        throw e;
+    }, 1);
+});
