4f6e86796377648b63a773e62e768a70510cde98
Chris Gomez <akagomez@gmail.com>
2016-01-08 12:34:38 -0500

    0.0.12

---
+++ dist/cjs/can-derive.js
@@ -0,0 +1,7 @@
+/*can-derive@0.0.11#can-derive*/
+var List = require('./list/list.js');
+var derivePlugin = { List: List };
+if (typeof window !== 'undefined' && !require.resolve && window.can) {
+    window.can.derive = derivePlugin;
+}
+module.exports = derivePlugin;
